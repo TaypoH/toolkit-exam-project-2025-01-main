c89e89fa37ec045a093fc2bbe277186f0515f624
TaypoH <merkantiln@gmail.com>
2025-07-01 15:58:26 +0300

    Fix warning componentWillReceiveProps

--- client/src/components/CreatorDashboard/CreatorDashboard.jsx
+++ client/src/components/CreatorDashboard/CreatorDashboard.jsx
@@ -88,9 +88,9 @@ class CreatorDashboard extends React.Component {
     );
   };
 
-  componentWillReceiveProps (nextProps, nextContext) {
-    if (nextProps.location.search !== this.props.location.search) {
-      this.parseUrlForParams(nextProps.location.search);
+  componentDidUpdate (prevProps) {
+    if (this.props.location.search !== prevProps.location.search) {
+      this.parseUrlForParams(this.props.location.search);
     }
   }
 
