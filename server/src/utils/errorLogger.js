a4f04735650810ed1a28b08ec77645f81e888d5a
TaypoH <merkantiln@gmail.com>
2025-07-14 15:10:09 +0300

    Refactor errorLogger on async/await

--- server/src/utils/errorLogger.js
+++ server/src/utils/errorLogger.js
@@ -1,21 +1,20 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const logFilePath = path.join(__dirname, '../../logs/error.log');
 
-function logError ({ message, code, stackTrace }) {
+async function logError ({ message, code, stackTrace }) {
   const errorObject = {
     message,
     time: Date.now(),
     code,
     stackTrace,
   };
-
-  fs.appendFile(logFilePath, JSON.stringify(errorObject) + '\n', err => {
-    if (err) {
-      console.error('Failed to write error log:', err);
-    }
-  });
+  try {
+    await fs.appendFile(logFilePath, JSON.stringify(errorObject) + '\n');
+  } catch (err) {
+    console.error('Failed to write error log:', err);
+  }
 }
 
 module.exports = logError;
