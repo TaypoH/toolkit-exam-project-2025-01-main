66158dccb720ea2dc4b7610aeb63c6f5c03b853f
TaypoH <merkantiln@gmail.com>
2025-07-29 12:44:24 +0300

    Refactor: convert contest components to functional with lifecycle hooks

--- client/src/components/ContestsContainer/ContestsContainer.jsx
+++ client/src/components/ContestsContainer/ContestsContainer.jsx
@@ -1,44 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './ContestContainer.module.sass';
 import Spinner from '../Spinner/Spinner';
 
-class ContestsContainer extends React.Component {
-  componentDidMount () {
-    window.addEventListener('scroll', this.scrollHandler);
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('scroll', this.scrollHandler);
-  }
-
-  scrollHandler = () => {
+const ContestsContainer = props => {
+  const scrollHandler = () => {
     const scrollTop = document.documentElement.scrollTop;
     const scrollHeight = document.documentElement.scrollHeight;
     const clientHeight = document.documentElement.clientHeight;
 
     if (scrollTop + clientHeight >= scrollHeight - 200) {
-      if (this.props.haveMore && !this.props.isFetching) {
-        this.props.loadMore(this.props.children.length);
+      if (props.haveMore && !props.isFetching) {
+        props.loadMore(props.children.length);
       }
     }
   };
 
-  render () {
-    const { isFetching } = this.props;
-    if (!isFetching && this.props.children.length === 0) {
-      return <div className={styles.notFound}>Nothing not found</div>;
-    }
-    return (
-      <div>
-        {this.props.children}
-        {isFetching && (
-          <div className={styles.spinnerContainer}>
-            <Spinner />
-          </div>
-        )}
-      </div>
-    );
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler);
+
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, [props.haveMore, props.isFetching, props.children.length, props.loadMore]);
+
+  const { isFetching } = props;
+  if (!isFetching && props.children.length === 0) {
+    return <div className={styles.notFound}>Nothing not found</div>;
   }
-}
+
+  return (
+    <div>
+      {props.children}
+      {isFetching && (
+        <div className={styles.spinnerContainer}>
+          <Spinner />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ContestsContainer;
